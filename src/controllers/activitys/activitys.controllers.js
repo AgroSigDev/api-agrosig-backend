76701e238bd7ce089d6d1df42bb941618cd11cdf
davidch <david087hdz@gmail.com>
2025-09-09 15:50:16 -0600

    update: se hizo mejoras a la implementacion de regitrar actividades con insumos, se hizo la parte de crear un helper de unidades para permitir al usuario registrar insumos con diferentes precios

--- src/controllers/activitys/activitys.controllers.js
+++ src/controllers/activitys/activitys.controllers.js
@@ -1,7 +1,7 @@
 import { Activitys } from '../../models/index.js'
 
 async function registerActivity (userId, cropId, activityData, inputs) {
-  const data = await Activitys.createActivityWithnputs(userId, cropId, activityData, inputs)
+  const data = await Activitys.createActivityWithInputs(userId, cropId, activityData, inputs)
   return data
 }
 
