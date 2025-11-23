468beb3e5f0fd02151f91769daf86a45e552d190
davidch <david087hdz@gmail.com>
2025-11-23 15:28:11 -0600

    Cambios para render

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv'
 
-const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.local'
-dotenv.config({ path: envFile })
+const env = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.local'
+dotenv.config({ path: env })
 
 /**
  * Application configuration object.
